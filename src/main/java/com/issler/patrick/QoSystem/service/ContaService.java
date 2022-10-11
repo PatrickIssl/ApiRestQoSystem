ea648a96aac87b37c56a3062603c649121abf346
patrickissl <patrickissler1234@gmail.com>
2022-10-11 15:08:45 -0300

    correções pedidas pela elisangela

--- src/main/java/com/issler/patrick/QoSystem/service/ContaService.java
+++ src/main/java/com/issler/patrick/QoSystem/service/ContaService.java
@@ -68,15 +68,15 @@ public class ContaService {
 		if(contas.getSenha().length() < 1){
 			return new ResponseEntity<>("Sua senha tem que ter mais de 6 caracteres", HttpStatus.BAD_REQUEST);
 		}
-
 		if (conta.isPresent()) {
-			contas.setPessoa(conta.get().getPessoa());
 			if(contas.getPessoa().getCargo().getId()!= null){
 				Optional<Cargo> cargo =  cargoRepository.findById(contas.getPessoa().getCargo().getId());
 				if(cargo.isPresent()){
-					contas.getPessoa().setCargo(cargo.get());
+					conta.get().getPessoa().setCargo(cargo.get());
 				}
+				System.out.println(cargo);
 			}
+			contas.setPessoa(conta.get().getPessoa());
 			contaRepository.save(contas);
 			return new ResponseEntity<Conta>(contas, HttpStatus.OK);
 		}
