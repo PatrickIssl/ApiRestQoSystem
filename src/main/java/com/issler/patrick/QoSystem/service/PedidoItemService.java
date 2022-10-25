5318386a44f1d5c9546eda318beb2e3e2a8df077
patrickissl <patrickissler1234@gmail.com>
2022-10-25 14:15:34 -0300

    Editar status do pedido arrumado

--- src/main/java/com/issler/patrick/QoSystem/service/PedidoItemService.java
+++ src/main/java/com/issler/patrick/QoSystem/service/PedidoItemService.java
@@ -19,83 +19,94 @@ import javax.swing.text.html.Option;
 @Service
 public class PedidoItemService {
 
-	@Autowired
-	PedidoItemRepository pedidoITemRepository;
+    @Autowired
+    PedidoItemRepository pedidoITemRepository;
 
-	@Autowired
-	MesaRepository mesaRepository;
+    @Autowired
+    MesaRepository mesaRepository;
 
-	@Autowired
-	PedidoRepository pedidoRepository;
+    @Autowired
+    PedidoRepository pedidoRepository;
 
-	@Autowired
-	ItemRepository itemRepository;
+    @Autowired
+    ItemRepository itemRepository;
 
-	public ResponseEntity<String> delete(PedidoItem pedidoITems) {
-		Optional<PedidoItem> pedidoITem = pedidoITemRepository.findById(pedidoITems.getId());
-		if (pedidoITem.isPresent()) {
-			pedidoITemRepository.delete(pedidoITem.get());
-			return new ResponseEntity<>("PedidoItem deletado com sucesso", HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>("PedidoItem não encontrado", HttpStatus.NOT_FOUND);
-		}
-	}
+    public ResponseEntity<String> delete(PedidoItem pedidoITems) {
+        Optional<PedidoItem> pedidoITem = pedidoITemRepository.findById(pedidoITems.getId());
+        if (pedidoITem.isPresent()) {
+            pedidoITemRepository.delete(pedidoITem.get());
+            return new ResponseEntity<>("PedidoItem deletado com sucesso", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("PedidoItem não encontrado", HttpStatus.NOT_FOUND);
+        }
+    }
 
-	public ResponseEntity<?> buscar(PedidoItem pedidoITem) {
-		Optional<PedidoItem> pedidoITems = pedidoITemRepository.findById(pedidoITem.getId());
-		if (pedidoITems.isPresent()) {
-			return new ResponseEntity<PedidoItem>(pedidoITems.get(), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>("PedidoItem não encontrado", HttpStatus.NOT_FOUND);
-		}
-	}
+    public ResponseEntity<?> buscar(PedidoItem pedidoITem) {
+        Optional<PedidoItem> pedidoITems = pedidoITemRepository.findById(pedidoITem.getId());
+        if (pedidoITems.isPresent()) {
+            return new ResponseEntity<PedidoItem>(pedidoITems.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("PedidoItem não encontrado", HttpStatus.NOT_FOUND);
+        }
+    }
 
-	public ResponseEntity<?> findAllByMesa(PedidoItem pedidoITem) {
-		Optional<Mesa> mesa = mesaRepository.findById(pedidoITem.getPedido().getMesa().getId());
-		if(!mesa.isPresent()){
-			return new ResponseEntity<>("mesa não encontrada", HttpStatus.NOT_FOUND);
-		}
-		List<PedidoItem> pedidoITems = pedidoITemRepository.findAllByPedidoMesa(mesa.get());
-		if (!pedidoITems.isEmpty()) {
-			return new ResponseEntity<List<PedidoItem>>(pedidoITems, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>("PedidoItem não encontrado", HttpStatus.NOT_FOUND);
-		}
-	}
-	public ResponseEntity<?> put(PedidoItem pedidoItems) {
-		Optional<PedidoItem> pedidoItem = pedidoITemRepository.findById(pedidoItems.getId());
-		if (pedidoItem != null) {
-			pedidoITemRepository.save(pedidoItems);
-			return new ResponseEntity<PedidoItem>(pedidoItems, HttpStatus.OK);
-		}
-		return new ResponseEntity<>("PedidoItem não encontrado", HttpStatus.NOT_FOUND);
-	}
+    public ResponseEntity<?> findAllByMesa(PedidoItem pedidoITem) {
+        Optional<Mesa> mesa = mesaRepository.findById(pedidoITem.getPedido().getMesa().getId());
+        if (!mesa.isPresent()) {
+            return new ResponseEntity<>("mesa não encontrada", HttpStatus.NOT_FOUND);
+        }
+        List<PedidoItem> pedidoITems = pedidoITemRepository.findAllByPedidoMesa(mesa.get());
+        if (!pedidoITems.isEmpty()) {
+            return new ResponseEntity<List<PedidoItem>>(pedidoITems, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("PedidoItem não encontrado", HttpStatus.NOT_FOUND);
+        }
+    }
 
-	public ResponseEntity<?> save(PedidoItem pedidoITems) {
-		Optional<Item> item = itemRepository.findById(pedidoITems.getItem().getId());
-		Optional<Pedido> pedido = pedidoRepository.findById(pedidoITems.getPedido().getId());
-		if (item.isPresent() && pedido.isPresent()){
-			pedidoITems.setPedido(pedido.get());
-			pedidoITems.setItem(item.get());
-			pedidoITemRepository.save(pedidoITems);
-			return new ResponseEntity<PedidoItem>(pedidoITems, HttpStatus.OK);
-		}else {
-			return new ResponseEntity<>("Erro ao encontrar pedido/item", HttpStatus.NOT_FOUND);
-		}
+    public ResponseEntity<?> put(PedidoItem pedidoItems) {
+        Optional<PedidoItem> pedidoItem = pedidoITemRepository.findById(pedidoItems.getId());
+        if (pedidoItem != null) {
+            pedidoITemRepository.save(pedidoItems);
+            return new ResponseEntity<PedidoItem>(pedidoItems, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("PedidoItem não encontrado", HttpStatus.NOT_FOUND);
+    }
 
-	}
+    public ResponseEntity<?> save(PedidoItem pedidoITems) {
+        if (pedidoITems.getItem() != null) {
+            Optional<Item> item = itemRepository.findById(pedidoITems.getItem().getId());
+            if (item.isPresent()) {
+                pedidoITems.setItem(item.get());
+            } else {
+                return new ResponseEntity<>("Erro ao encontrar item", HttpStatus.NOT_FOUND);
+            }
 
-	public ResponseEntity<?> findAll() {
-		return new ResponseEntity<>(pedidoITemRepository.findAll(), HttpStatus.OK);
-	}
+        }
+        if (pedidoITems.getPedido() != null) {
+            Optional<Pedido> pedido = pedidoRepository.findById(pedidoITems.getPedido().getId());
+            if (pedido.isPresent()) {
+                pedidoITems.setPedido(pedido.get());
+            }else {
+				return new ResponseEntity<>("Erro ao encontrar pedido", HttpStatus.NOT_FOUND);
+			}
+        }
+        pedidoITemRepository.save(pedidoITems);
+        return new ResponseEntity<PedidoItem>(pedidoITems, HttpStatus.OK);
 
-	public ResponseEntity<?> findAllByPedido(Pedido pedido) {
-		Optional<Pedido> pedidos = pedidoRepository.findById(pedido.getId());
-		if(pedidos.isPresent()){
-			Optional<List<PedidoItem>> pedidoItem = pedidoITemRepository.getAllByPedido(pedidos);
-			return new ResponseEntity<>(pedidoItem, HttpStatus.OK);
-		}else {
-			return new ResponseEntity<>("Pedido não encontrado", HttpStatus.NOT_FOUND);
-		}
-	}
+
+    }
+
+    public ResponseEntity<?> findAll() {
+        return new ResponseEntity<>(pedidoITemRepository.findAll(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> findAllByPedido(Pedido pedido) {
+        Optional<Pedido> pedidos = pedidoRepository.findById(pedido.getId());
+        if (pedidos.isPresent()) {
+            Optional<List<PedidoItem>> pedidoItem = pedidoITemRepository.getAllByPedido(pedidos);
+            return new ResponseEntity<>(pedidoItem, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Pedido não encontrado", HttpStatus.NOT_FOUND);
+        }
+    }
 }
